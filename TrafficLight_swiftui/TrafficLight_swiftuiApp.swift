c3f395d71ab8a5116af6081d93c622a2adbfc4d0
Samir <imnotajedi@icloud.com>
2022-10-18 14:08:15 +0600

    Initial Commit

---
+++ TrafficLight_swiftui/TrafficLight_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_swiftuiApp.swift
+//  TrafficLight_swiftui
+//
+//  Created by Самир Кафаров on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
